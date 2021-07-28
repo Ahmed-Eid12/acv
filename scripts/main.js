aa07643e68b7ca4af6c97bebe42825a1229a9a1f
Ahmed-Eid12 <ahmedzidan757@gmail.com>
2021-07-28 23:48:03 +0200

    CV ahmedeid

---
+++ scripts/main.js
@@ -0,0 +1,117 @@
+(function() {
+  "use strict";
+
+  /**
+   * calculate age 
+   * ========================================
+   */
+   const date1 = new Date('2/24/1993');
+   const date2 = new Date();
+   const diffTime = Math.abs(date2 - date1);
+   const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+   
+   const age = Math.floor(diffDays/365);
+   document.getElementById('age').innerText = age;
+   // ========================================
+
+   /**
+    * send me an email 
+    * contact me
+    */
+  
+
+  window.addEventListener('load', () => {
+    on_page_load()
+  });
+
+  /**
+   * Function gets called when page is loaded.
+   */
+  function on_page_load() {
+    // Initialize On-scroll Animations
+    AOS.init({
+      anchorPlacement: 'top-left',
+      duration: 600,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,
+      disable: 'mobile'
+    });
+  }
+
+  /**
+   * Navbar effects and scrolltop buttons upon scrolling
+   */
+  const navbar = document.getElementById('header-nav')
+  var body = document.getElementsByTagName("body")[0]
+  const scrollTop = document.getElementById('scrolltop')
+  window.onscroll = () => {
+    if (window.scrollY > 0) {
+      navbar.classList.add('fixed-top', 'shadow-sm')
+      body.style.paddingTop = navbar.offsetHeight + "px"
+      scrollTop.style.visibility = "visible";
+      scrollTop.style.opacity = 1;
+    } else {
+      navbar.classList.remove('fixed-top', 'shadow-sm')
+      body.style.paddingTop = "0px"
+      scrollTop.style.visibility = "hidden";
+      scrollTop.style.opacity = 0;
+    }
+  };
+
+  /**
+   * Masonry Grid
+   */
+  var elem = document.querySelector('.grid');
+  if(elem) {
+    imagesLoaded(elem, function() {
+      new Masonry(elem, {
+        itemSelector: '.grid-item',
+        percentPosition: true,
+        horizontalOrder: true
+      });
+    })
+  }
+
+  /**
+   * Big Picture Popup for images and videos
+   */
+   document.querySelectorAll("[data-bigpicture]").forEach((function(e) {
+     e.addEventListener("click", (function(t){
+       t.preventDefault();
+       const data =JSON.parse(e.dataset.bigpicture)
+       BigPicture({
+        el: t.target,
+        ...data
+      })
+     })
+    )
+  }))
+
+  /**
+   * Big Picture Popup for Photo Gallary
+   */
+   document.querySelectorAll(".bp-gallery a").forEach((function(e) {
+    var caption = e.querySelector('figcaption')
+    var img = e.querySelector('img')
+    // set the link present on the item to the caption in full view
+    img.dataset.caption = '<a class="link-light" target="_blank" href="' + e.href + '">' + caption.innerHTML + '</a>';
+     e.addEventListener("click", (function(t){
+       t.preventDefault();
+       BigPicture({
+        el: t.target,
+        gallery: '.bp-gallery',
+      })
+     })
+    )
+  }))
+
+})();
+
+// open cv in popup
+let windowObjectReference;
+const windowFeatures = "menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes";
+
+function openRequestedPopup() {
+  windowObjectReference = window.open("/cv.html", "CNN_WindowName", windowFeatures);
+}
